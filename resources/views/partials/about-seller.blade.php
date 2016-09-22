555f999d4179abf5fc48830d494e88995ef73955
MS\Marcus <marcus.stratu@gmail.com>
2016-09-22 15:22:33 +0300

    Fix conflict;

--- resources/views/partials/about-seller.blade.php
+++ resources/views/partials/about-seller.blade.php
@@ -40,157 +40,117 @@
     </div>
 </div>
 
-<<<<<<< HEAD
 @section('scripts')
     @if(Auth::user())
         <script type="text/javascript">
-            $(function () {
-                var like_btn = $('span[data-type=like]');
-                var dislike_btn = $('span[data-type=dislike]');
+        $(function(){
+            var ajax_running = false;
+            var like_btn = $('span[data-type=like]');
+            var dislike_btn = $('span[data-type=dislike]');
+            
+            $('.set_vote').click(function() {
+                var $this = $(this);
+                var count = $(this).find('span');
 
-                $('.set_vote').click(function () {
-                    var $this = $(this);
-                    var count = $(this).find('span');
-
+                if(!ajax_running) {
+                    ajax_running = true; 
                     $.ajax({
                         type: 'post',
                         url: $this.data('action'),
-                        data: {vendor: "{{$vendor->slug}}", like_type: $this.data('type')},
-                        success: function (response) {
+                        data: { vendor : "{{$vendor->slug}}", like_type : $this.data('type') },
+                        success: function(response)
+                        {
                             var out = JSON.parse(response);
                             like_btn.find('span').html(out.likes);
                             dislike_btn.find('span').html(out.dislikes);
+                            $('.likes_count').html(out.likes_count);
+                            $('.likes_percent').html(out.likes_percent);
+                            if(out.was_liked == 'like')
+                            {
+                                $('.like').addClass('vote_active');
+                                $('.unlike').removeClass('vote_active');
+                            }
+                            if(out.was_liked == 'unlike') 
+                            { 
+                                $('.like').removeClass('vote_active');
+                                $('.unlike').addClass('vote_active');
+                            }
+                            if(out.was_liked == 'not_liked')
+                            {
+                                $('.like').removeClass('vote_active');
+                                $('.unlike').removeClass('vote_active');
+                            }
+                            
+                        },
+                        complete: function (){
+                            ajax_running = false;
                         }
                     });
-                });
-            });
+                }
+           });
+        });    
         </script>
     @else
         <script>
-            $('.set_vote').click(function () {
-                $('#modal').openModal();
-                $('.modal-trigger').leanModal({
-                    dismissible: true,
-                    opacity: .5,
-                    in_duration: 300,
-                    out_duration: 200,
-                });
+        $('.set_vote').click(function() {
+            $('#modal').openModal(); 
+            $('.modal-trigger').leanModal({
+                dismissible: true, 
+                opacity: .5, 
+                in_duration: 300, 
+                out_duration: 200, 
             });
+        });
         </script>
     @endif
-=======
-@section('js')
-@if(Auth::user())
-    <script type="text/javascript">
-    $(function(){
-        var ajax_running = false;
-        var like_btn = $('span[data-type=like]');
-        var dislike_btn = $('span[data-type=dislike]');
-        
-        $('.set_vote').click(function() {
-            var $this = $(this);
-            var count = $(this).find('span');
 
-            if(!ajax_running) {
-                ajax_running = true; 
+    <script>
+    $('.auth_submit_ajax, .auth_register_ajax').on("click", function(event){
+            event.preventDefault();
+            var form = $(this).parents('form').serialize();
+            var response = $(this).parents('form').find('.response');
+            var authtype = $(this).parents('form').data('authtype');
+            var btn = $(this);
+            $('.auth_errors li').remove(); 
+                btn.attr("disabled", true);
                 $.ajax({
-                    type: 'post',
-                    url: $this.data('action'),
-                    data: { vendor : "{{$vendor->slug}}", like_type : $this.data('type') },
-                    success: function(response)
-                    {
-                        var out = JSON.parse(response);
-                        like_btn.find('span').html(out.likes);
-                        dislike_btn.find('span').html(out.dislikes);
-                        $('.likes_count').html(out.likes_count);
-                        $('.likes_percent').html(out.likes_percent);
-                        if(out.was_liked == 'like')
-                        {
-                            $('.like').addClass('vote_active');
-                            $('.unlike').removeClass('vote_active');
+                    url : $(this).parents('form').data('action'),
+                    method : 'POST',
+                    dataType : 'json',
+                    data : form,
+                    success : function(data){
+                        var wrap = $('form[data-authtype='+ authtype + '] .auth_errors');
+
+                        if(data.errors){
+                            $.each(data.errors, function(input_name, errors){
+                                if($.isArray(errors)){
+                                    $.each(errors, function(k, error){
+                                       wrap.append('<li>'+ error +'</li>');
+                                    });
+                                }
+                                $('form[data-authtype='+ authtype + '] input[name='+input_name+']').css('border','1px solid red');
+                                setTimeout(function()
+                                { 
+                                    $('.auth_errors li').remove(); 
+                                }, 5000);
+                            }); 
+                            return;    
                         }
-                        if(out.was_liked == 'unlike') 
-                        { 
-                            $('.like').removeClass('vote_active');
-                            $('.unlike').addClass('vote_active');
+
+                        if(data.redirect){
+                            window.location.href = data.redirect;
+                            return;
                         }
-                        if(out.was_liked == 'not_liked')
-                        {
-                            $('.like').removeClass('vote_active');
-                            $('.unlike').removeClass('vote_active');
-                        }
-                        
+                       wrap.append('<li style="background:#f5f5f5; border:1px solid green; color:green;">Login Success!</li>');
+                        location.reload();
+                        return;
                     },
-                    complete: function (){
-                        ajax_running = false;
+                    complete : function(){
+                        if(btn.attr("disabled")){
+                            btn.attr("disabled", false);
+                        }
                     }
                 });
-            }
-       });
-    });    
-    </script>
-@else
-    <script>
-    $('.set_vote').click(function() {
-        $('#modal').openModal(); 
-        $('.modal-trigger').leanModal({
-            dismissible: true, 
-            opacity: .5, 
-            in_duration: 300, 
-            out_duration: 200, 
         });
-    });
     </script>
-@endif
-
-<script>
-$('.auth_submit_ajax, .auth_register_ajax').on("click", function(event){
-        event.preventDefault();
-        var form = $(this).parents('form').serialize();
-        var response = $(this).parents('form').find('.response');
-        var authtype = $(this).parents('form').data('authtype');
-        var btn = $(this);
-        $('.auth_errors li').remove(); 
-            btn.attr("disabled", true);
-            $.ajax({
-                url : $(this).parents('form').data('action'),
-                method : 'POST',
-                dataType : 'json',
-                data : form,
-                success : function(data){
-                    var wrap = $('form[data-authtype='+ authtype + '] .auth_errors');
-
-                    if(data.errors){
-                        $.each(data.errors, function(input_name, errors){
-                            if($.isArray(errors)){
-                                $.each(errors, function(k, error){
-                                   wrap.append('<li>'+ error +'</li>');
-                                });
-                            }
-                            $('form[data-authtype='+ authtype + '] input[name='+input_name+']').css('border','1px solid red');
-                            setTimeout(function()
-                            { 
-                                $('.auth_errors li').remove(); 
-                            }, 5000);
-                        }); 
-                        return;    
-                    }
-
-                    if(data.redirect){
-                        window.location.href = data.redirect;
-                        return;
-                    }
-                   wrap.append('<li style="background:#f5f5f5; border:1px solid green; color:green;">Login Success!</li>');
-                    location.reload();
-                    return;
-                },
-                complete : function(){
-                    if(btn.attr("disabled")){
-                        btn.attr("disabled", false);
-                    }
-                }
-            });
-    });
-</script>
->>>>>>> develop
 @endsection
